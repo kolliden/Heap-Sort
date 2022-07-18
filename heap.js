a18cad3a8fe4296e08e6ee068917e41564a19a17
kolliden <58950501+kolliden@users.noreply.github.com>
2022-07-18 19:53:12 +0200

    added heap class

---
+++ heap.js
@@ -0,0 +1,17 @@
+class MaxHeap {
+    constructor(){
+        this.heap = [];
+    }
+
+    parentIndex(index){
+        return Math.floor((index-1)/2);
+    }
+
+    leftChildIndex(index){
+        return (2*index + 1);
+    }
+
+    rightChildIndex(index){
+        return (2*index + 2);
+    }
+}
